00e8e49eb7684795e9f73f8c5a79a64b33f16856
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2025-11-01 16:48:18 -0600

    feat(package): added react and vue in the proyect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
 
 export default defineConfig({
-  integrations: [tailwind()],
- 
+  integrations: [
+    tailwind(),
+    react(),
+    vue()
+  ],
 });
