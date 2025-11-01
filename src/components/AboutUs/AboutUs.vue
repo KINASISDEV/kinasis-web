5ee3bc919d28b19c07d68ea5db69cba40126a898
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2025-11-01 16:48:52 -0600

    feat(AboutUs): added finally page AbuttUs

---
+++ src/components/AboutUs/AboutUs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about-container">
+    <div class="about-section">
+        <h1 class="title-aboutUs">Sobre Nosotros</h1>
+        <p class="parrafo">Bienvenido a la página de About Us de KINASIS. Aquí puedes conocer más sobre nuestra misión, visión y el equipo que hace posible todo lo que hacemos.</p>
+    </div>
+    <div class="right-align">
+        <div class="about-section">
+            <h1 class="title-aboutUs">Nuestra Visión</h1>
+            <p class="parrafo">Nuestra visión es ser líderes en el sector, ofreciendo soluciones innovadoras y de alta calidad que satisfagan las necesidades de nuestros clientes.</p>
+        </div>
+    </div>
+    <div class="about-section">
+        <h1 class="title-aboutUs">Nuestras Metas</h1>
+        <p class="parrafo">Nuestras metas incluyen expandir nuestra presencia en el mercado, mejorar continuamente nuestros productos y servicios, y fomentar un ambiente de trabajo positivo para nuestro equipo.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import './AboutUs.css';
+</script>
