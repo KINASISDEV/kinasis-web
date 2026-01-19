88eea27e3f15d5f4872ffee65ff142c1d098f25c
AvellanaSkywalker <emanuel3628@outlook.com>
2026-01-19 13:56:20 -0600

    feat(team):agrego foto grupal

--- src/components/Foundators/Foundators.vue
+++ src/components/Foundators/Foundators.vue
@@ -6,11 +6,11 @@
                 Los fundadores de <span class="brand-name">Kinasis</span> fueron visionarios que identificaron una oportunidad única en el panorama tecnológico global. Con valentía y determinación, decidieron dar el salto que transformaría sus aspiraciones en realidad.
             </p>
             <p class="foundators-paragraph">
-                Todo comenzó con una <strong>ambición temprana y arriesgada</strong>: crear soluciones que trascendieran las expectativas convencionales. Su compromiso inquebrantable con la <strong>innovación</strong> y la <strong>calidad</strong> se convirtió en los pilares fundamentales que nos definen hoy.
+                Todo comenzó ALOJA con una <strong>ambición temprana y arriesgada</strong>: crear soluciones que trascendieran las expectativas convencionales. Su compromiso inquebrantable con la <strong>innovación</strong> y la <strong>calidad</strong> se convirtió en los pilares fundamentales que nos definen hoy.
             </p>
         </div>
         <div class="foundators-image-wrapper">
-            <img src="/TeamMedia/foundators.png" alt="Fundadores Kinasis" class="foundators-image" />
+            <img src="/TeamMedia/kinasis_team.jpeg" alt="Fundadores Kinasis" class="foundators-image" />
         </div>
     </section>
 </template>
